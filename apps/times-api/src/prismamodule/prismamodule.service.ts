74e3d56eb9979c568eb19e4e114873e7cd394920
felipecaroca <felipe.caroca.dev@gmail.com>
2024-02-28 18:35:03 -0300

    feat: debug env

--- apps/times-api/src/prismamodule/prismamodule.service.ts
+++ apps/times-api/src/prismamodule/prismamodule.service.ts
@@ -7,5 +7,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect()
     console.log('conectado a la db prisma')
+    console.log(process.env.DATABASE_URL || 'no hay env')
   }
 }
